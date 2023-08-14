fd8341d19ba4cb0129e9253ec73273e7b74b0445
Yoshinori Kasamatsu <ka.sa.ma.123@gmail.com>
2023-08-15 01:57:24 +0900

    コード整理

--- rust/work_items/src/main.rs
+++ rust/work_items/src/main.rs
@@ -12,47 +12,40 @@ use std::fs::{File, create_dir_all};
 use std::io::Write;
 use crate::work_items::WorkItem;
 
+const THREAD_COUNT: usize = 100;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
+    // 引数処理
+    let (organization, project, pat) = match get_start_args() {
+        Ok(value) => value,
+        Err(value) => return value,
+    };
 
-    // 引数のチェック
-    if args.len() < 4 {
-        println!("引数が足りません。");
-        println!("Usage: {} <organization> <project> <pat>", args[0]);
-        return Ok(());
-    }
-    
-    let organization = &args[1];
-    let project = &args[2];
-    let pat = &args[3];
+    // id一覧の取得
+    let ids = work_items::get_work_items(organization.as_str(), project.as_str(), pat.as_str()).await?;
 
-    let ids = work_items::get_work_items(organization, project, pat).await?;
-    let work_items = work_items::get_work_items_details(organization, project, pat, ids).await?;
+    // WorkItemsの取得
+    let work_items = work_items::get_work_items_details(organization.as_str(), project.as_str(), pat.as_str(), ids).await?;
+
     let mut save_results = Vec::new();
-    let mut task_count = 0;
+    save_results.push(Vec::new());
+
     // 結果の表示
     for item in &work_items {
         println!("{}\t{}\t{:?}\t{}\t{}\t{}", item.id, item.rev, item.fields.changed_date.unwrap(), item.fields.state, item.fields.work_item_type,  item.fields.title);
 
         // リビジョンのJSONを保存を並列実行する
-        let save_result = save_revisions_json(organization, project, pat, item);
-        if task_count == 0 {
-            let mut new_save_result = Vec::new();
-            save_results.push(new_save_result);
-        }
-
+        let save_result = save_revisions_json(organization.as_str(), project.as_str(), pat.as_str(), item);
         save_results.last_mut().unwrap().push(save_result);
-
-        task_count += 1;
-        if task_count == 200 {
-            task_count = 0;
+        if save_results.last().unwrap().len() == THREAD_COUNT {
+            save_results.push(Vec::new());
         }
-        
     }
 
     // save_resultsを逐次実行する
     for save_result in save_results {
+        println!("{}", save_result.len());
         let results: Vec<_> = futures::future::join_all(save_result).await;
         // 結果のチェック
         for result in results {
@@ -66,7 +59,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn save_revisions_json(organization: &String, project: &String, pat: &String, item: &WorkItem) -> Result<(), Box<dyn Error>> {
+fn get_start_args() -> Result<(String, String, String), Result<(), Box<dyn Error>>> {
+    let args: Vec<String> = env::args().collect();
+
+    // 引数のチェック
+    if args.len() < 4 {
+        println!("引数が足りません。");
+        println!("Usage: {} <organization> <project> <pat>", args[0]);
+        return Err(Ok(()));
+    }
+
+    // 引数を変数化
+    let organization = args[1].clone();
+    let project = args[2].clone();
+    let pat = args[3].clone();
+    Ok((organization, project, pat))
+}
+
+async fn save_revisions_json(organization: &str, project: &str, pat: &str, item: &WorkItem) -> Result<(), Box<dyn Error>> {
     let id = item.id;
     let json_text = work_items::get_work_item_revisions_json(organization, project, pat, id).await?;
     // テキストファイルに保存
